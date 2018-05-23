188e3031235b3570a31f7cc841a6798f3227a680
AurelienDER <34481271+AurelienDER@users.noreply.github.com>
2018-05-23 22:22:38 +0200

    Update traitement.h

--- traitement.h
+++ traitement.h
@@ -4,7 +4,10 @@ typedef struct{int l;int c;} COUPLE;
 typedef struct{int inf;int sup;} BORNES;
 
 int nb_erreurs_image(unsigned int** matrice);
+//Compte le nombre de pixel aberrants dans la matrice
 unsigned int** moyenne_matrice(unsigned int*** tab_matrice,int nb_matrice);
+//Realise la moyenne  de n matrice. Les valeurs aberrantes sont supprimiees
 unsigned int** profondeur_to_altitude(unsigned int** matrice_profondeur,int nl,int nc);
+//Recree un autre matrice correspondant a la transformation d'une matrice de profondeur vers une matrice d'altitude
 
 #endif
