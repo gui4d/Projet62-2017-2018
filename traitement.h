71dbf8ef4c58a55b02108cf38be8bb5d1898c9ab
AurelienDER <34481271+AurelienDER@users.noreply.github.com>
2018-05-23 22:17:40 +0200

    Update traitement.h

--- traitement.h
+++ traitement.h
@@ -7,13 +7,4 @@ int nb_erreurs_image(unsigned int** matrice);
 unsigned int** moyenne_matrice(unsigned int*** tab_matrice,int nb_matrice);
 unsigned int** profondeur_to_altitude(unsigned int** matrice_profondeur,int nl,int nc);
 
-COUPLE calcul_gradient_point(unsigned int** matrice,int i,int j);
-COUPLE** cree_matrice_gradient(unsigned int** matrice);
-int calcul_gradient_ligne(COUPLE** matrice_gradient,int numero,char mode);
-int calcul_gradient_droite(COUPLE** matrice_gradient,COUPLE p1,COUPLE p2);
-int trouve_bord_haut(COUPLE** matrice_gradient);
-int trouve_bord_bas(COUPLE** matrice_gradient);
-int trouve_bord_gauche(COUPLE** matrice_gradient);
-int trouve_bord_droit(COUPLE** matrice_gradient);
-
 #endif
