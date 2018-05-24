5786037d36a07f72ed0f9fa2c93a652caf6c96d9
gui4d <38852724+gui4d@users.noreply.github.com>
2018-05-24 11:37:04 +0200

    Update calibrage.h

--- calibrage.h
+++ calibrage.h
@@ -1,5 +1,7 @@
 #include<SDL2/SDL.h>
 
 
-unsigned int** initialisation_manuel_relief(int nl_Final,int nc_Final);
+unsigned int** initialisation_manuel_relief(int nl_Final,int nc_Final);//fonction goblale: appel les fontions de matrice.c afin de:
+                                                                       //capturer les images du kinect, interagir avec l utilisateur pour trouver les bords du bac, aporter diverses transformations (deformations optiques :voir les commentaires du code)
+
 
